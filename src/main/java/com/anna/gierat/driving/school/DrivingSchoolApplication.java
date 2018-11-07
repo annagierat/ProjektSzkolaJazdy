606d0fb65b051785903d3de9dd72b38eaac4424c
annagierat <anna.folwarska.88@gmail.com>
2018-11-07 12:03:46 +0100

    first commit

---
+++ src/main/java/com/anna/gierat/driving/school/DrivingSchoolApplication.java
@@ -0,0 +1,12 @@
+package com.anna.gierat.driving.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivingSchoolApplication.class, args);
+	}
+}
